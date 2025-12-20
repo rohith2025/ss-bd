0fdd479ef00e9ca81028f0921368e0bee86175d8
rohith2025 <yelurirohith2025@gmail.com>
2025-12-20 21:43:05 +0530

    server.js done

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import studentRoutes from "./routes/studentRoutes.js";
+import teacherRoutes from "./routes/teacherRoutes.js";
+import parentRoutes from "./routes/parentRoutes.js";
+import attendanceRoutes from "./routes/attendanceRoutes.js";
+import examRoutes from "./routes/examRoutes.js";
+import feesRoutes from "./routes/feesRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import noticeRoutes from "./routes/noticeRoutes.js";
+import holidayRoutes from "./routes/holidayRoutes.js";
+import thesisRoutes from "./routes/thesisRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+import activitiesRoutes from "./routes/activitiesRoutes.js";
+import timetableRoutes from "./routes/timetableRoutes.js";
+
 dotenv.config();
-
-const app = express();
-
 connectDB();
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/teacher", teacherRoutes);
+app.use("/api/parent", parentRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/exams", examRoutes);
+app.use("/api/fees", feesRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/notices", noticeRoutes);
+app.use("/api/holidays", holidayRoutes);
+app.use("/api/thesis", thesisRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/activities", activitiesRoutes);
+app.use("/api/timetable", timetableRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
