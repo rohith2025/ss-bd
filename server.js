ae57986cfe93836d47ed4ede5258423066d23371
rohith2025 <yelurirohith2025@gmail.com>
2025-12-20 20:31:50 +0530

    db-connection-success

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-app.listen(3000 , () => {
-    console.log('server started ')
-})
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
